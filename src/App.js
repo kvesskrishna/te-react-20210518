7787bf0fdc2dbbd0903f40420eff181e89a95381
Krishna KVESS <krishna.kkarthi@gmail.com>
2021-05-19 11:16:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+const doubleNum = x => 2 * x;
 
-function App() {
+const App = () => {
+  const technology = 'React';
+  const today = new Date();
+  let a = 7;
+  let b = 9;
+  // let x = doubleNum(a);
+  // let y = doubleNum(b);
+  const mystyle = { textShadow: '5px 5px 5px red', fontSize: 34, color: 'red' };
+  const mystyle2 = { textShadow: '5px 5px 5px green', fontSize: 34, color: 'green' };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < h2>My First {technology} Program</h2>
+      <p>This is my para</p>
+      <p>{today.getFullYear()} Expression </p>
+      <h3 style={(a + b) % 2 === 0 ? mystyle : mystyle2} id='before'>The sum of a and b before doubling is {a + b}</h3>
+      <h3 className='result' id='after'>The sum of a and b after doubling is {doubleNum(a) + doubleNum(b)}</h3>
     </div>
   );
 }
